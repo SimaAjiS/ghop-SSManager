cf69b100cfa6037e1657177f0797d05a6f616175
SimaAjiS <comb.yasushi+github@gmail.com>
2025-11-22 22:54:31 +0900

    refactor: uvicornのアプリケーション参照を直接インポートしたappオブジェクトに変更

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import uvicorn
 
+from backend.main import app
+
 if __name__ == "__main__":
     # Run the application using uvicorn
-    # "backend.app:app" tells uvicorn to look for the 'app' object in 'backend/app.py'
-    uvicorn.run("backend.app:app", host="0.0.0.0", port=8000, reload=True)
+    # "app:app" tells uvicorn to look for the 'app' object in this file (app.py)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
