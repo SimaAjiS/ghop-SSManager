b0e87e0b6d9d30e1fd25979181a324cc786d3df2
SimaAjiS <comb.yasushi+github@gmail.com>
2025-11-27 01:36:41 +0900

    Release v1.0.0: Refactoring, documentation update, and cleanup

--- app.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-
-if __name__ == "__main__":
-    # Run the application using uvicorn
-    # "app:app" tells uvicorn to look for the 'app' object in this file (app.py)
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
